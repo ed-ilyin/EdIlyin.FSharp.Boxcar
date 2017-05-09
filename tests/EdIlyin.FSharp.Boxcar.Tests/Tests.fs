d31efe22d388b4f6e1bd9005ca99aea86194b8b0
Ed Ilyin <ed.il@live.com>
2017-05-09 17:25:11 +0300

    initial commit

---
+++ tests/EdIlyin.FSharp.Boxcar.Tests/Tests.fs
@@ -0,0 +1,11 @@
+module EdIlyin.FSharp.Boxcar.Tests
+
+open EdIlyin.FSharp.Boxcar
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  // let result = Library.hello 42
+  let result = 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
